481e8266bdb02ec7bbaa5f7d349a94d1c1a15b2c
Jerry Guerra <jerry.gdr13@gmail.com>
2022-07-11 22:10:05 -0600

    Create fortuneCookie.py

---
+++ fortuneCookie.py
@@ -0,0 +1,15 @@
+import random
+
+dic = ['It is certain','It is decidedly so','Without a doubt','Yes, definitely','You may rely on it','As I see it, yes','Most likely','Outlook good','Yes','Signs point to yes','Reply hazy try again','Ask again later','Better not tell you now','Cannot predict now','Concentrate and ask again','Don\'t count on it','My reply is no','My sources say no','Outlook not so good','Very doubtful']
+
+x = random.randint(0,19)
+
+print("You can ask me anything and I will answer!! go ahead!!")
+
+question = input('Please type your question :  ')
+
+if question:
+  print(dic[x])
+else:
+  print('You didn\t asked anything yet!!')
+  exit()
